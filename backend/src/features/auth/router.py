387e4d21d8dd01431e31179ba32c611a0d5b16fd
amine <aaliane781@gmail.com>
2026-08-02 14:09:37 +0200

    update(auth-improv): updated the router
    Updated files:
    
    backend/src/features/auth/router.py

--- backend/src/features/auth/router.py
+++ backend/src/features/auth/router.py
@@ -21,6 +21,8 @@ router = APIRouter()
 # Auto-generates /register
 router.include_router(fastapi_users.get_register_router(UserRead, UserCreate))
 
+router.include_router(fastapi_users.get_verify_router(UserRead))
+
 router.include_router(
     fastapi_users.get_users_router(UserRead, UserUpdate),
     prefix="/users",
