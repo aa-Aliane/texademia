e2506652a9299ad4e34ec22549f3e45d7141c540
amine <aaliane781@gmail.com>
2026-08-21 15:08:00 +0200

    fix: login redirection to redaction if logged
    Updated files:
    
    frontend/src/routes/login.tsx

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -3,16 +3,12 @@ import { getCookieHeader } from "#/shared/api/serverCookie";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
-  beforeLoad: async ({ context }) => {
+  beforeLoad: async ({ context: { queryClient } }) => {
     try {
-      const user = await context.queryClient.ensureQueryData(
-        currentUserQueryOptions(getCookieHeader())
-      );
-
-      if (user) {
-        throw redirect({ to: "/redaction" });
-      }
-    } catch (error) {
+      await queryClient.ensureQueryData(currentUserQueryOptions(getCookieHeader()));
+      throw redirect({ to: "/redaction" });
+    }
+     catch (error) {
       if (error instanceof Error && "isRedirect" in error) {
         throw error;
       }
